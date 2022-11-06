406b080fc4e80b7b269e80c5be52fba9fc2d9c54
Matthew Akuhata <matthewakuhata@gmail.com>
2022-11-07 10:36:54 +1100

    Create Core app

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+"""
+Calulator functions 
+"""
+def add(x, y): 
+    return x + y
+
+def subtrack(x, y):
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+"""
+Sample Tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+    
+    def test_subtrack_numbers(self):
+        res = calc.subtrack(10, 15)
+
+        self.assertEqual(res, 5)
