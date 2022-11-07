b5cebbda5a00c93de9a735c57a7468ac95b16c9b
Matthew Akuhata <matthewakuhata@gmail.com>
2022-11-07 17:17:21 +1100

    Add normailisation for user emails

--- app/core/models.py
+++ app/core/models.py
@@ -10,7 +10,7 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and returns a new user"""
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -24,5 +24,5 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
-    
+
     USERNAME_FIELD = 'email'
