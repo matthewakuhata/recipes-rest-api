55b49d8382ce1fc1f9397430a6b602f21196f6d3
Matthew Akuhata <matthewakuhata@gmail.com>
2022-11-08 14:06:03 +1100

    Add: Unit test for getting recipe details

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -7,16 +7,22 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
+
 from rest_framework import status
 from rest_framework.test import APIClient
 
+
 from core.models import Recipe
 
-from recipe.serializers import RecipeSerializer
+
+from recipe.serializers import (RecipeSerializer, RecipeDetailSerializer)
 
 
 RECIPES_URL = reverse('recipe:recipe-list')
 
+def detail_irl(recipe_id):
+    """Create recipe detail URL"""
+    return reverse('recipe:recipe-detail', args=[recipe_id])
 
 def create_recipe(user, **params):
     """Create and return a sample recipe."""
@@ -83,4 +89,14 @@ class PrivateRecipeApiTests(TestCase):
         recipes = Recipe.objects.filter(user=self.user)
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_get_recipe_detail(self):
+        """Test get recipe detail"""
+        recipe = create_recipe(user=self.user)
+
+        url = detail_url(recipe.id)
+        res = self.client.get(url)
+
+        serializer = RecipeDetailSerializer(recipe)
         self.assertEqual(res.data, serializer.data)
