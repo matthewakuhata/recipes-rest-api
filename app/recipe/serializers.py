c1e80c8dfbe4ffa51f1c9f3ac4c0d3ffd2ccd511
Matthew Akuhata <matthewakuhata@gmail.com>
2022-11-09 10:30:22 +1100

    Update: Recipe view with ingredients field

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -26,10 +26,14 @@ class TagSerializer(serializers.ModelSerializer):
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
     tags = TagSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
+        fields = [
+            'id', 'title', 'time_minutes', 'price', 'link', 'tags',
+            'ingredients',
+        ]
         read_only_fields = ['id']
 
     def _get_or_create_tags(self, tags, recipe):
