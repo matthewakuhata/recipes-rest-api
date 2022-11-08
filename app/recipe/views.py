2838081630e0490add022a2915544ea64c80d6b9
Matthew Akuhata <matthewakuhata@gmail.com>
2022-11-09 09:57:40 +1100

    Add: Ingredients view set

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,7 +6,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 
-from core.models import Recipe, Tag
+from core.models import (Recipe, Tag, Ingredient)
 from recipe import serializers
 
 
@@ -46,5 +46,20 @@ class TagViewSet(
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        """Retrieve recipes for authenticated user."""
+        """Retrieve tags for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+    """Manage tags in the database"""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve Ingredients for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
