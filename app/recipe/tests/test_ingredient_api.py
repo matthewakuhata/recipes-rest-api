f65eb90fa6c7436d9211a69cc471db7238c92638
Matthew Akuhata <matthewakuhata@gmail.com>
2022-11-09 10:28:33 +1100

    Add: Update/Delete endpoint unit tests for Ingredients

--- app/recipe/tests/test_ingredient_api.py
+++ app/recipe/tests/test_ingredient_api.py
@@ -19,6 +19,10 @@ from recipe.serializers import IngredientSerializer
 INGREDIENTS_URL = reverse('recipe:ingredient-list')
 
 
+def detail_url(tag_id):
+    return reverse('recipe:ingredient-detail', args=[tag_id])
+
+
 def create_user(email='example.com@example.com', password='test123'):
     return get_user_model().objects.create_user(email=email, password=password)
 
@@ -68,3 +72,26 @@ class PrivateTagsAPITest(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], expected.name)
         self.assertEqual(res.data[0]['id'], expected.id)
+
+    def test_update_ingredient(self):
+        """Test update ingredient"""
+        ingredient = Ingredient.objects.create(user=self.user, name='Kale')
+
+        payload = {'name': 'Lettuce'}
+        url = detail_url(ingredient.id)
+        res = self.client.patch(url, payload)
+
+        ingredient.refresh_from_db()
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(ingredient.name, payload['name'])
+
+    def test_delete_ingredient(self):
+        """Test delete ingredient"""
+        ingredient = Ingredient.objects.create(user=self.user, name='Kale')
+
+        url = detail_url(ingredient.id)
+        res = self.client.delete(url)
+
+        ingredients = Ingredient.objects.filter(user=self.user)
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(ingredients.exists())
